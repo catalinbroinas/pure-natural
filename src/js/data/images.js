61b418254843418fc5088116012a5bb546c2db58
catalinbroinas <catalinbroinas@yahoo.com>
2025-07-30 16:53:46 +0300

    feat(images.js): add images object for products
    - Include image paths for all product cards

---
+++ src/js/data/images.js
@@ -0,0 +1,18 @@
+// Card images
+import intenseRomance from "../../assets/img/products/product-1.webp";
+import flowerPower from "../../assets/img/products/product-2.webp";
+import citricJoy from "../../assets/img/products/product-3.webp";
+import noMosquitos from "../../assets/img/products/product-4.webp";
+import deepDisinfect from "../../assets/img/products/product-5.webp";
+import aromatherapy from "../../assets/img/products/product-6.webp";
+
+const images = {
+    intenseRomance: { cardImage: intenseRomance },
+    flowerPower: { cardImage: flowerPower },
+    citricJoy: { cardImage: citricJoy },
+    noMosquitos: { cardImage: noMosquitos },
+    deepDisinfect: { cardImage: deepDisinfect },
+    aromatherapy: { cardImage: aromatherapy }
+};
+
+export default images;
