5009f9f30b5650f1f8dfd6391feed4983a720276
catalinbroinas <catalinbroinas@yahoo.com>
2025-07-30 20:08:52 +0300

    feat(ProductCard): add category-based icon

--- src/js/components/ProductCard.jsx
+++ src/js/components/ProductCard.jsx
@@ -3,6 +3,14 @@ import { Ripple, initMDB } from "mdb-ui-kit/js/mdb.es.min";
 
 function ProductCard({ product }) {
     const { image: productImage, name, category, price } = product;
+    const icons = {
+        'Parfum': 'fa-leaf',
+        'Spray anti-țânțari': 'fa-spray-can',
+        'Dezinfectant': 'fa-pump-medical',
+        'Aromaterapie': 'fa-spa'
+    };
+    const DEFAULT_ICON = 'fa-leaf';
+    const iconProduct = icons[category.trim()] || DEFAULT_ICON;
 
     useEffect(() => {
         initMDB({ Ripple });
@@ -36,7 +44,7 @@ function ProductCard({ product }) {
             <div className="card-body d-flex flex-column">
                 <h3 className="card-title mb-2">{name}</h3>
                 <h4 className="card-sub-title mb-2 mt-auto">
-                    <i className="fa-solid fa-leaf text-muted me-2"></i>
+                    <i className={`fa-solid ${iconProduct} text-muted me-2`} aria-hidden="true"></i>
                     {category}
                 </h4>
                 <p className="price m-0">{price} lei</p>
