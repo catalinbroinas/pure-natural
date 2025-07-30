7e5bf4bbb63b2a7010a926c2094050a7b069e519
catalinbroinas <catalinbroinas@yahoo.com>
2025-07-30 14:53:05 +0300

    chore: add React and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import imagemin from 'vite-plugin-imagemin';
+import react from '@vitejs/plugin-react';
 
 export default {
     server: {
@@ -44,6 +45,7 @@ export default {
                     { cleanupIDs: false }
                 ]
             }
-        })
+        }),
+        react()
     ],
 }
