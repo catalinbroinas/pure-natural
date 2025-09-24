1ab9d39734970bc9f98b7cf39efbc33125959f6e
catalinbroinas <catalinbroinas@yahoo.com>
2025-09-24 18:40:56 +0300

    style(ProductModalBody): improve layout and spacing

--- src/js/components/product-modal/ProductModalBody.jsx
+++ src/js/components/product-modal/ProductModalBody.jsx
@@ -12,7 +12,7 @@ function ProductModalBody({
 
     return (
         <div className="modal-body">
-            <div className="row align-items-center gx-lg-5">
+            <div className="row align-items-center gx-xl-5 g-lg-4 g-md-5 g-4">
                 <ProductName image={imageSrc} name={name} />
                 <ProductDetails 
                     amount={amount}
@@ -27,21 +27,22 @@ function ProductModalBody({
 
 function ProductName({ image, name }) {
     return (
-        <div className="col-lg-5 mb-5 mb-lg-0">
-            <div 
-                className="bg-image ripple rounded-6 shadow-5-strong"
+        <div className="col-xl-5 col-lg-6 col-12">
+            <div
+                className="bg-image rounded-6 shadow-5-strong"
                 data-mdb-ripple-init
                 data-mdb-ripple-color="light"
             >
                 <img
                     src={image}
                     className="w-100"
+                    alt=""
                 />
 
                 <div className="mask mask-strong-dark">
                     <div className="d-flex justify-content-center align-items-center h-100">
                         <div className="px-4 py-5 px-md-5 text-white text-center">
-                            <h3 className="text-uppercase fw-bold mb-4">
+                            <h3 className="text-uppercase fw-bold m-0">
                                 {name}
                             </h3>
                         </div>
@@ -103,25 +104,27 @@ function ProductDetails({
     ].filter(Boolean);
 
     return (
-        <div className="col-lg-7 mb-4 mb-lg-0">
-           {details.map((item, index) => (
-            <ProductDetailsItem
-                key={index}
-                iconClass={item.icon}
-                title={item.title}
-                text={item.text}
-            />
-           ))}
+        <div className="col-xl-7 col-lg-6 col-12">
+            <div className="d-flex flex-column gap-xl-5 gap-lg-4 gap-md-5 gap-4">
+                 {details.map((item, index) => (
+                    <ProductDetailsItem
+                        key={index}
+                        iconClass={item.icon}
+                        title={item.title}
+                        text={item.text}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
 
 function ProductDetailsItem({ iconClass, title, text }) {
     return (
-        <div className="d-flex align-items-start mb-5">
+        <div className="d-flex align-items-start">
             <div className="flex-shrink-0">
                 <div className="rounded-4 shadow-2-strong bg-accent p-3">
-                    <i className={`fa-solid ${iconClass} fa-lg text-white fa-fw`}></i>
+                    <i className={`fa-solid ${iconClass} text-white fa-lg fa-fw`}></i>
                 </div>
             </div>
 
