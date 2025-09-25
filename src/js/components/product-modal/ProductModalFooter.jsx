3e80ef19dc974b46c7c8086013ff035e1e9290bc
catalinbroinas <catalinbroinas@yahoo.com>
2025-09-25 17:13:32 +0300

    style(btn-secondary): add custom SCSS mixin

--- src/js/components/product-modal/ProductModalFooter.jsx
+++ src/js/components/product-modal/ProductModalFooter.jsx
@@ -4,9 +4,9 @@ function ProductModalFooter() {
         <div className="modal-footer">
             <button
                 type="button"
-                className="btn btn-secondary btn-rounded"
+                className="btn btn-secondary btn-secondary-custom btn-rounded"
                 data-mdb-ripple-init
-                data-mdb-ripple-color="light"
+                data-mdb-ripple-color="#2C5F2D"
                 data-mdb-dismiss="modal"
             >
                 Închide
