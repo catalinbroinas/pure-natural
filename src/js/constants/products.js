69feedb645503a0bf09bab629cbd70bb46ab1306
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-28 16:51:06 +0300

    content(products): refine category names

--- src/js/constants/products.js
+++ src/js/constants/products.js
@@ -3,8 +3,8 @@ export const PRODUCT_CATEGORY = {
 	PERFUME: "Parfum",
 	DISINFECTANT: "Dezinfectant",
 	AROMATHERAPY: "Aromaterapie",
-	MOSQUITO_SPRAY: "Spray anti-insecte",
-	MOSQUITO_BITE_BALSAM: "Spray anti-țânțari"
+	MOSQUITO_SPRAY: "Spray împotriva insectelor",
+	MOSQUITO_BITE_BALSAM: "Balsam împotriva țânțarilor"
 };
 
 export const PRODUCT_DEFAULT_ICON = 'fa-leaf';
